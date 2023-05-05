dbffa896145be7bdebcba9a2541677d96cc810a4
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2023-05-05 08:52:41 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using COMP_2084_Lesson_01.Models;
+using Humanizer;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace COMP_2084_Lesson_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.CurrentTime = DateTime.Now;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
